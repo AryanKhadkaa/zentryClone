25d40c7c8f75d5b02d82426921ab1059f8d33f09
AryanKhakdaa <khadkaaryan809@gmail.com>
2025-02-04 23:33:01 +0545

    added animated title effect

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import React from "react";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
+import { AnimatedTitle } from "./animatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 const About = () => {
@@ -10,7 +11,7 @@ const About = () => {
       scrollTrigger: {
         trigger: "#clip",
         start: "center center",
-        end: "bottom center",
+        end: "+=800 center",
         scrub: 0.5,
         pin: true,
         pinSpacing: true,
@@ -25,21 +26,23 @@ const About = () => {
   });
 
   return (
-    <div className="about min-h-screen w-screen">
+    <div className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase md:text-[15px]">
           Welcome to Zentry
         </h2>
-        <div className="mt-5 special-font text-4xl md:text-[6rem] leading-[0.8] hero-heading text-center">
-          DISC <b>O</b>VER THE WORLD'S LARGEST SHARED <b>A</b>DVENTURE
-        </div>
+
+        <AnimatedTitle
+          title={"DISC<b>O</b>VER THE WORLD'S LARGEST SHARED <b>A</b>DVENTURE"}
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
           <p>Zentry unites every player from countless games and platforms</p>
         </div>
       </div>
-      <div className="h-dvh w-screen" id="clip">
+
+      <div className="h-dvh w-screen bg-red-500" id="clip">
         <div className="mask-clip-path about-image">
           <img
             src="img/about.webp"
--- src/components/animatedTitle.jsx
+++ src/components/animatedTitle.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useEffect, useRef } from "react";
 
-const animatedTitle = () => {
-  return <div>animatedTitle</div>;
+export const AnimatedTitle = ({ title }) => {
+  //const AnimatedTitle = () => {};
+
+  const containerRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play non none reverse",
+          markers: true,
+        },
+      });
+
+      titleAnimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3D(0,0,0) rotateY(0deg) rotateX(0deg)",
+        ease: "power2.inOut",
+        stagger: 0.02,
+      });
+    }, containerRef);
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <div ref={containerRef} className="animated-title">
+      <div className="mt-5 animated-word special-font font-semibold px-2 mx-auto text-4xl md:text-[6rem] leading-[0.8] text-center">
+        {/* {title.split("<br/>").map((line, idx) => {
+        return <div>
+
+            </div>
+        })} */}
+        DISC<b>O</b>VER THE WORLD'S LARGEST SHARED <b>A</b>DVENTURE
+      </div>
+    </div>
+  );
 };
-
-export default animatedTitle;
+AnimatedTitle;
